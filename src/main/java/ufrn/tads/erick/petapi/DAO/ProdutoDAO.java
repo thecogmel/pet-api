ab7cf9285deeaf78ef4777e59b9d2a786df45fe6
Erick Medeiros <erick_mmedeiros@hotmail.com>
2021-07-05 17:57:30 -0300

    config api classes

---
+++ src/main/java/ufrn/tads/erick/petapi/DAO/ProdutoDAO.java
@@ -0,0 +1,76 @@
+package ufrn.tads.erick.petapi.DAO;
+
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import ufrn.tads.erick.petapi.model.Produto;
+import ufrn.tads.erick.petapi.controllers.ConectaBanco;
+
+public class ProdutoDAO {
+    public List<Produto> listarProdutos() {
+
+        Connection connection = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        List<Produto> listaDeProdutos = new ArrayList<>();
+
+        try {
+            connection = ConectaBanco.getConnection();
+
+            stmt = connection.prepareStatement("select * from produto");
+
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Produto p = new Produto();
+
+                p.setId(rs.getInt("id"));
+                p.setNome(rs.getString("nome"));
+                p.setRaca(rs.getString("raca"));
+                p.setPreco(rs.getFloat("preco"));
+                p.setEspecie(rs.getString("especie"));
+                p.setMarca(rs.getString("marca"));
+                p.setCategoria(rs.getString("categoria"));
+
+                listaDeProdutos.add(p);
+
+            }
+
+        } catch (SQLException | URISyntaxException ex) {
+            // response.getWriter().append("Connection Failed! Check output console");
+        }
+
+        return listaDeProdutos;
+    }
+
+    public void cadastrarProduto(Produto p) {
+        Connection connection = null;
+        PreparedStatement stmt = null;
+
+        try {
+            connection = ConectaBanco.getConnection();
+
+            stmt = connection.prepareStatement(
+                    "insert into produto (nome, raca, preco, especie, marca, categoria) values (?,?,?,?,?,?)");
+
+            stmt.setString(1, p.getNome());
+            stmt.setString(2, p.getRaca());
+            stmt.setFloat(3, p.getPreco());
+            stmt.setString(4, p.getEspecie());
+            stmt.setString(5, p.getMarca());
+            stmt.setString(6, p.getCategoria());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException | URISyntaxException ex) {
+            // response.getWriter().append("Connection Failed! Check output console");
+        }
+
+    }
+}
--- src/main/java/ufrn/tads/erick/petapi/controllers/HelloController.java
+++ src/main/java/ufrn/tads/erick/petapi/controllers/HelloController.java
@@ -1,15 +1,51 @@
 package ufrn.tads.erick.petapi.controllers;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class HelloController {
-    @GetMapping(value="/")
-    public String getHello(@RequestParam String mensagem) {
-        return new String(mensagem);
+    @GetMapping
+    public void getHello(HttpServletRequest request, HttpServletResponse response) throws IOException {
+         // TODO Auto-generated method stub
+         String name = request.getParameter("nome");
+         if (name != null) {
+             response.getWriter().println("Bem vindo a pet-api, " + name + ".");   
+         } else {
+            response.getWriter().println("Bem vindo a pet-api, anonimo.");
+         }
+ 
+         Connection connection = null;
+         try {
+             connection = ConectaBanco.getConnection();
+         } catch (SQLException | URISyntaxException ex) {
+             response.getWriter().println("Connection Failed! Check output console");
+         }
+ 
+         if (connection != null) {
+             response.getWriter().println("A conexao com o banco foi realizada!");
+         } else {
+             response.getWriter().println("A conexao com o banco falhou!");
+         }
+ 
+         try {
+             assert connection != null;
+             connection.close();
+         } catch (SQLException e) {
+             // TODO Auto-generated catch block
+             e.printStackTrace();
+         }
     }
 }
