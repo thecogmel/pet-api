55dce86b20a3ed2c2660cc0767d893d8c2af58db
Erick Medeiros <erick_mmedeiros@hotmail.com>
2021-07-05 22:14:35 -0300

    remove lombok

--- src/main/java/ufrn/tads/erick/petapi/model/Produto.java
+++ src/main/java/ufrn/tads/erick/petapi/model/Produto.java
@@ -1,14 +1,66 @@
 package ufrn.tads.erick.petapi.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
 public class Produto {
-    @Getter @Setter private int id;
-    @Getter @Setter private String nome;
-    @Getter @Setter private String raca;
-    @Getter @Setter private String especie;
-    @Getter @Setter private String marca;
-    @Getter @Setter private float preco;
-    @Getter @Setter private String categoria;
+    private int id;
+    private String nome;
+    private String raca;
+    private String especie;
+    private String marca;
+    private float preco;
+    private String categoria;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
